16876e75721513e959e7a06662dc3880c9655c17
ojuliocode <juliojuriollif@gmail.com>
2024-07-26 14:04:33 -0300

    header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,7 +1,23 @@
 import "./Header.css"
-
+import { AccountCircle, CheckCircle, Person4Rounded } from "@mui/icons-material"
+import { Person2Rounded } from "@mui/icons-material"
 function Header(){
-    return <h1> Header funciona </h1>
+    return(
+        <header className="header">
+            <section className="titulo">
+
+            <CheckCircle color="success" sx={{fontSize: 50}}></CheckCircle>
+            <h1>Tarefas App</h1>
+            </section>
+            <section className="perfil">
+                <div>
+                    <h3>João césar</h3>
+                    <span>joaocesar@gmail.com</span>
+                </div>
+                <AccountCircle sx={{fontSize: 50}}></AccountCircle>
+            </section>
+        </header>
+    )
 }
 
 export default Header
